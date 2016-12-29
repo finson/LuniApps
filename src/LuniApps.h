ffa63859bbb5002981783a4f6d47c784eff52178
Doug Johnson <strix@whidbey.com>
2016-12-28 23:50:52 -0800

    Add Arduino library include file for LuniApps.
    Provided so that LuniApps can be used as a library in the Arduino IDE,
    regardless of whether or not there are other available include files in
    the top-level src directory.

---
+++ src/LuniApps.h
@@ -0,0 +1,8 @@
+#ifndef LuniApps_h
+#define LuniApps_h
+
+// Arduino library tag file.  Provided so that LuniApps can be
+// used as a library in the Arduino IDE, regardless of whether or not
+// there are other available include files in the top-level src directory.
+
+#endif
